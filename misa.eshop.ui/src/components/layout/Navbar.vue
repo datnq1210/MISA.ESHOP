e4b9dfbbc17ac459cc40eef8899299afea501144
Đạt Nguyễn Quốc <datnq1210@gmail.com>
2021-03-01 01:10:24 +0700

    ve layout

---
+++ misa.eshop.ui/src/components/layout/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navbar{
+    width: 200px;
+    height: 100vh;
+    background: #333;
+    position: fixed;
+}
+</style>
